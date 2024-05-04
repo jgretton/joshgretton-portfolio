2b613f7d95b136864d2dc3f79e6ad2e90a535494
Joshua Gretton <jb.gretton@googlemail.com>
2024-05-04 20:32:49 +0100

    Header added to layout

---
+++ components/header.js
@@ -0,0 +1,120 @@
+"use client";
+
+import Link from "next/link";
+import React, { useState, Fragment } from "react";
+import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import {
+  ChevronDownIcon,
+  PhoneIcon,
+  PlayCircleIcon,
+} from "@heroicons/react/20/solid";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+const links = [
+  {
+    title: "About",
+    link: "/about",
+  },
+  {
+    title: "Contact",
+    link: "/",
+  },
+  {
+    title: "Live Projects",
+    link: "/#liveProjects",
+  },
+  {
+    title: "Projects",
+    link: "/#projects",
+  },
+];
+const Header = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  return (
+    <header className="fixed inset-x-0 top-0 mx-auto bg-white  px-6 py-6 xl:container sm:px-16 ">
+      <div className="row flex items-center justify-between">
+        <Link href="/" className="text-4xl">
+          JG <span className="text-blue-300"> / </span>
+        </Link>
+        <nav className="hidden sm:block">
+          <Link href="/about" className="mx-10">
+            About
+          </Link>
+          <Link href="/" className="mx-10">
+            Contact
+          </Link>
+        </nav>
+        <div className="flex sm:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setIsMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+      </div>
+
+      <Dialog
+        className="lg:hidden"
+        open={isMobileMenuOpen}
+        onClose={setIsMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            {/* <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt=""
+              />
+            </a> */}
+            <Link href="/" className="text-4xl">
+              JG <span className="text-blue-300"> / </span>
+            </Link>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setIsMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {links.map((link, index) => (
+                  <Link
+                    href={link.link}
+                    key={index}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    {link.title}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  );
+};
+
+export default Header;
