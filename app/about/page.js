a85d67641632d9badee14bfa6ab149e5215c3bb1
Joshua Gretton <jb.gretton@googlemail.com>
2024-06-12 21:25:23 +0100

    Visual bugs

--- app/about/page.js
+++ app/about/page.js
@@ -15,7 +15,7 @@ import { aboutMeText } from "@/data/about";
 const AboutPage = () => {
   return (
     // <div className="relative h-full w-full px-12">
-    <div className="mx-auto px-6 pb-10 xl:container sm:px-12">
+    <>
       <Hero>
         Hi! - My name is <span className="text-blue-300">Josh</span>. <br />
         I&apos;m a <span className="text-blue-300">Developer </span> based
@@ -23,26 +23,28 @@ const AboutPage = () => {
         in the UK.
       </Hero>
 
-      <section className="relative mt-[60dvh] h-full w-full bg-white dark:bg-[#15202b]">
-        <div className="z-10 mb-32 w-full bg-white dark:bg-[#15202b]">
-          <div className="flex flex-col gap-14 md:flex-row">
-            <div className="w-6/6 top-28 self-center sm:h-1/2 md:sticky md:self-auto lg:h-full lg:w-1/3">
-              <Image
-                src={PhotoOfMe}
-                alt="Photo of me"
-                className="h-1/3 rounded-2xl object-contain md:w-full lg:h-1/3 lg:object-cover lg:object-top"
-                placeholder="blur"
-              />
-            </div>
-            <div className="relative mt-5 h-auto w-full lg:w-2/3">
-              <Markdown className="prose min-w-full font-light tracking-wide dark:prose-invert prose-headings:font-light prose-h2:text-2xl prose-strong:font-normal">
-                {aboutMeText.markdown}
-              </Markdown>
+      <div className="bg-white pb-10">
+        <section className="relative mx-auto mt-[60dvh] h-full w-full bg-white px-4 xl:container dark:bg-[#15202b] sm:px-12">
+          <div className="z-10 mb-32 w-full bg-white dark:bg-[#15202b]">
+            <div className="flex flex-col gap-14 md:flex-row">
+              <div className="w-6/6 top-28 self-center sm:h-1/2 md:sticky md:self-auto lg:h-full lg:w-1/3">
+                <Image
+                  src={PhotoOfMe}
+                  alt="Photo of me"
+                  className="h-1/3 rounded-2xl object-contain md:w-full lg:h-1/3 lg:object-cover lg:object-top"
+                  placeholder="blur"
+                />
+              </div>
+              <div className="relative mt-5 h-auto w-full lg:w-2/3">
+                <Markdown className="prose min-w-full font-light tracking-wide dark:prose-invert prose-headings:font-light prose-h2:text-2xl prose-strong:font-normal">
+                  {aboutMeText.markdown}
+                </Markdown>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </>
   );
 };
 
--- app/projects/[slug]/page.js
+++ app/projects/[slug]/page.js
@@ -49,58 +49,52 @@ const Page = ({ params }) => {
     project;
 
   return (
-    <div className="mx-auto px-6 pb-10 xl:container sm:px-12">
-      {/* <p>back</p> */}
+    <>
       <Hero back>{title}</Hero>
-      {/* <div
-        className={` fixed inset-x-0 top-0 -z-10 mx-auto flex h-[60svh] transform items-center bg-white px-4 pt-12 transition-all xl:container sm:px-16`}
-      >
-        <h2 className="text-2xl font-light tracking-widest sm:text-4xl md:text-5xl">
-          <span className="text-blue-300">{title}</span>
-        </h2>
-      </div> */}
-      <section className="relative mt-[60dvh] h-full w-full bg-white dark:bg-[#15202b]">
-        <div className="sm:h-134 group relative h-80 w-full cursor-pointer overflow-hidden">
-          <Image
-            src={image}
-            alt={image_alt}
-            className="h-full w-full rounded-t-lg object-cover object-top"
-            layout="fill"
-            // placeholder="blur"
-          />
-        </div>
-        <div className="relative flex flex-col md:flex-row">
-          <div className="top-32 mt-20 h-fit w-full md:sticky md:w-1/4">
-            <p className="text-base font-medium">Project Name</p>
-            <p className="mb-5 ml-2 text-sm font-light">{title}</p>
+      <div className="bg-white pb-10">
+        <section className="relative mx-auto mt-[60dvh] h-full w-full bg-white px-4 xl:container dark:bg-[#15202b] sm:px-12">
+          <div className="sm:h-134 group relative h-80 w-full cursor-pointer overflow-hidden">
+            <Image
+              src={image}
+              alt={image_alt}
+              className="h-full w-full rounded-t-lg object-cover object-top"
+              layout="fill"
+              // placeholder="blur"
+            />
+          </div>
+          <div className="relative flex flex-col md:flex-row">
+            <div className="top-32 mt-20 h-fit w-full md:sticky md:w-1/4">
+              <p className="text-base font-medium">Project Name</p>
+              <p className="mb-5 ml-2 text-sm font-light">{title}</p>
 
-            <p className="text-base font-medium">Technologies used</p>
-            <ul className="ml-2">
-              {tags.map((tag, index) => (
-                <li className="text-sm font-light" key={index}>
-                  {tag}
-                </li>
-              ))}
-            </ul>
-            <Link
-              href={live_href}
-              target="_blank"
-              className="sm:text-md group mb-3 mt-5 inline-flex items-center gap-2 self-start text-base leading-6 tracking-wide text-gray-700 decoration-2 underline-offset-2 hover:underline dark:text-white"
-            >
-              See the live project
-              <ArrowTopRightOnSquareIcon className="size-4 transition-transform group-hover:-translate-y-0.5 group-hover:translate-x-0.5" />
-            </Link>
+              <p className="text-base font-medium">Technologies used</p>
+              <ul className="ml-2">
+                {tags.map((tag, index) => (
+                  <li className="text-sm font-light" key={index}>
+                    {tag}
+                  </li>
+                ))}
+              </ul>
+              <Link
+                href={live_href}
+                target="_blank"
+                className="sm:text-md group mb-3 mt-5 inline-flex items-center gap-2 self-start text-base leading-6 tracking-wide text-gray-700 decoration-2 underline-offset-2 hover:underline dark:text-white"
+              >
+                See the live project
+                <ArrowTopRightOnSquareIcon className="size-4 transition-transform group-hover:-translate-y-0.5 group-hover:translate-x-0.5" />
+              </Link>
+            </div>
+            <div className="mt-20 grid flex-1 gap-10">
+              {/* text-2xl font-light tracking-wide transition-colors sm:text-3xl */}
+              <Markdown className="prose min-w-full font-light tracking-wide dark:prose-invert prose-headings:font-light prose-h2:text-2xl prose-strong:font-normal prose-img:rounded-lg">
+                {/* <Markdown className="prose min-w-full font-light tracking-wide dark:prose-invert prose-img:rounded-lg"> */}
+                {markdown}
+              </Markdown>
+            </div>
           </div>
-          <div className="mt-20 grid flex-1 gap-10">
-            {/* text-2xl font-light tracking-wide transition-colors sm:text-3xl */}
-            <Markdown className="prose min-w-full font-light tracking-wide dark:prose-invert prose-headings:font-light prose-h2:text-2xl prose-strong:font-normal prose-img:rounded-lg">
-              {/* <Markdown className="prose min-w-full font-light tracking-wide dark:prose-invert prose-img:rounded-lg"> */}
-              {markdown}
-            </Markdown>
-          </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </>
   );
 };
 
