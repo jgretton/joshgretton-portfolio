cfab8bae0b31c6c2a0ceb2ee6369637cacbb3840
Joshua Gretton <jb.gretton@googlemail.com>
2024-06-13 09:24:00 +0100

    removed unused code

--- components/hero.js
+++ components/hero.js
@@ -27,32 +27,3 @@ const Hero = ({ children, back }) => {
 };
 
 export default Hero;
-// import { ArrowLeftIcon } from "@heroicons/react/24/outline";
-// import Link from "next/link";
-// import React from "react";
-
-// const Hero = ({ children, back }) => {
-//   return (
-//     <section
-//       className={`fixed inset-x-0 top-0 -z-10 mx-auto mt-5 flex h-[60dvh] items-center bg-white px-4 pt-12 xl:container dark:bg-[#15202b] sm:px-12`}
-//     >
-//       {back && (
-//         <Link
-//           href={"/"}
-//           className="group absolute top-28 inline-flex items-center gap-2"
-//         >
-//           <ArrowLeftIcon
-//             className="size-5 text-gray-800 transition-transform group-hover:-translate-x-1 dark:text-white"
-//             aria-label="hidden"
-//           />
-//           back
-//         </Link>
-//       )}
-//       <span className="text-[min(7vw,1.85rem)] font-light leading-relaxed tracking-widest sm:text-4xl sm:leading-relaxed md:text-5xl md:leading-relaxed">
-//         {children}
-//       </span>
-//     </section>
-//   );
-// };
-
-// export default Hero;
--- data/about.js
+++ data/about.js
@@ -19,25 +19,3 @@ Outside of coding, I have a passion for volleyball and I am a qualified coach an
 
 `,
 };
-// export const aboutMeText = {
-//   markdown: `
-// ## About Me
-
-// Hey, I'm Josh, a dedicated self-taught front-end developer with a passion for building dynamic and responsive web applications. I possess a strong foundation in HTML, CSS, and JavaScript. Outside of coding, I have a deep passion for volleyball and am a qualified coach and referee. My travels through Southeast Asia have broadened my perspective and inspired my projects. I also enjoy exploring new cultures and staying active.
-
-// Hey, I'm Josh, a self-taught front-end developer.
-
-// ### My Journey
-
-// My path into web development began while working as a production worker at an engineering company. Driven by a fascination with the web, I devoted my evenings to learning coding through tutorials and hands-on projects. Over time, I developed proficiency in front-end technologies, particularly Next.js and Tailwind CSS.
-
-// Last year, I embarked on a transformative journey across Southeast Asia. During my travels, I established [BluButton Digital](https://www.blubuttondigital.co.uk/) to offer freelance web development services and continue advancing my skills. Among my notable projects is [Volleyscore](https://www.volleyscore.co.uk/basic-match), a volleyball scoreboard app I designed and developed.
-
-// ### Skills
-
-// - **Front-End Development:** HTML, CSS, JavaScript, React, Next.js, Tailwind CSS
-// - **Tools:** Git, GitHub, VS Code, Figma
-// - **Other Skills:** SEO, responsive design, basic UX/UI principles
-
-// `,
-// };
