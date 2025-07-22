55206d281bec05425273f715e72deb8562fd304b
Joshua Gretton <jb.gretton@googlemail.com>
2025-07-22 19:45:46 +0100

    Update page.tsx
    fixed bug with Link highlighting when hovering on the full paragraph rather than the link

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,6 +5,7 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 import { getPageBySlug } from "@/lib/content";
 import { notFound } from "next/navigation";
 import { generateJsonLd } from "@/lib/generateJsonld";
+import AnimatedSection from "@/components/AnimatedSection";
 
 export async function generateMetadata() {
   const page = getPageBySlug("about");
@@ -87,7 +88,7 @@ const AboutPage = () => {
       </Hero>
 
       <div className="dark:bg-dark bg-gray-50 pb-10">
-        <section className="dark:bg-dark relative mx-auto h-full w-full bg-gray-50 px-4 xl:container sm:px-12">
+        <AnimatedSection className="dark:bg-dark relative mx-auto h-full w-full bg-gray-50 px-4 xl:container sm:px-12">
           <div className="dark:bg-dark z-10 mb-32 w-full bg-gray-50">
             <div className="flex flex-col gap-14 md:flex-row">
               <div className="w-6/6 top-28 self-center sm:h-1/2 md:sticky md:self-auto lg:h-full lg:w-1/3">
@@ -99,13 +100,13 @@ const AboutPage = () => {
                 />
               </div>
               <article className="relative mt-5 h-auto w-full lg:w-2/3">
-                <div className="prose dark:prose-invert prose-headings:font-light prose-h2:text-2xl hover:prose-a:text-blue-500 prose-strong:font-normal hover:prose-a:dark:text-blue-500 min-w-full font-light tracking-wide">
+                <div className="prose dark:prose-invert prose-headings:font-light prose-h2:text-2xl prose-a:text-blue-500 prose-a:hover:text-blue-600 prose-strong:font-normal prose-a:dark:text-blue-500 prose-a:dark:hover:text-blue-400 min-w-full font-light tracking-wide">
                   <MDXRemote source={content} />
                 </div>
               </article>
             </div>
           </div>
-        </section>
+        </AnimatedSection>
       </div>
     </>
   );
